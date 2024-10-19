c8bfaa8c572c790fd181781792675639128545c7
Franciscs Soares <1221180@isep.ipp.pt>
2024-10-19 22:38:27 +0100

    criar classe ingredientes

---
+++ Cozinha/Model/Ingrediente.cs
@@ -0,0 +1,8 @@
+namespace REST_aurante.Cozinha.Model;
+
+public class Ingrediente
+{
+    public string Nome { get; set; }
+    public string CategoriaAlimenticia { get; set; }
+    public bool Ativo { get; set; }
+}
