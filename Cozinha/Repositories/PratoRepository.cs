c433f4945ed3f065233e2952a7689ee51f361463
beamsteixeira <1220824@isep.ipp.pt>
2024-10-28 19:46:56 +0000

    feat: prato repository

--- Cozinha/Repositories/PratoRepository.cs
+++ Cozinha/Repositories/PratoRepository.cs
@@ -32,5 +32,42 @@ namespace Cozinha.Repositories{
         public async Task<List<Prato>> GetPratoByIngredienteNome(string nome) {
             return await _context.Pratos.Where(p => p.Ingredientes.Any(i => i.Nome.Equals(nome))).ToListAsync();
         }
+
+        public async Task<List<Prato>> GetPratosByState(bool IsAtivo)
+        {
+            return await _context.Pratos.Where(i => i.IsAtivo == IsAtivo).ToListAsync();
+        } 
+
+        public async Task<Prato> UpdatePrato(Prato prato)
+        {
+            _context.Entry(prato).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                return prato;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+        }
+
+        public async Task<Prato> AddPrato(Prato prato)
+        {
+            var newPrato = await _context.Pratos.AddAsync(prato);
+
+            await _context.SaveChangesAsync();
+            return newPrato.Entity;
+        }
+
+        public async Task<bool> Remove(Prato prato)
+        {
+            _context.Pratos.Remove(prato);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+
     } 
 }
