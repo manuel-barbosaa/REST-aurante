bda4ba75dab6141b1286c46162019fb4777b5fb1
beamsteixeira <1220824@isep.ipp.pt>
2024-10-28 19:58:06 +0000

    feat: prato service

--- Cozinha/Services/PratoService.cs
+++ Cozinha/Services/PratoService.cs
@@ -1,52 +1,63 @@
-// using Cozinha.Model.DTO;
-// using Cozinha.Model;
-// using Cozinha.Repositories;
+using Cozinha.Model.DTO;
+using Cozinha.Model;
+using Cozinha.Repositories;
 
-// namespace Cozinha.Services {
-//     public class PratoService {
+namespace Cozinha.Services {
+    public class PratoService {
 
-//         private CozinhaContext _context;
-//         private PratoRepository _repo;
+        private CozinhaContext _context;
+        private PratoRepository _repo;
 
-//         public PratoService(CozinhaContext context) {
-//             _context = context;
-//             _repo = new PratoRepository(_context);
-//         }
+        public PratoService(CozinhaContext context) {
+            _context = context;
+            _repo = new PratoRepository(_context);
+        }
 
-//         public async Task<List<ListarPratoDTO>> GetPratos() {
-//             List<Prato> Pratos = await _repo.GetPrato();
+        public async Task<List<ListarPratoDTO>> GetPratos() {
+            List<Prato> Pratos = await _repo.GetPrato();
 
-//             return Pratos.Select(x => PratoListItem(x)).ToList();
-//         }
+            return Pratos.Select(x => PratoListItem(x)).ToList();
+        }
 
-//         public async Task<DetalharPratoDTO?> GetPratoByValor(string value)
-//         {
-//             Prato? Prato;
+        public async Task<List<ListarPratoDTO>> GetPratosAtivosList()
+        {
+            List<Prato> allActivePratos = await _repo.GetPratosByState(true);
+        
+            return allActivePratos.Select(x => PratoListItem(x)).ToList();
+        }
 
-//             if (long.TryParse(value, out long id))
-//                 Prato = await _repo.GetPratoById(id);
-//             else
-//                 Prato = await _repo.GetPratoByNome(value);
+        public async Task<ListarPratoDTO> CreateNewPrato(CriarPratoDTO info)
+        {
+            Prato newPrato = new Prato
+            {
+                Id = info.Id,
+                Nome = info.Nome,
+                IsAtivo = info.IsAtivo,
+                TipoPrato = info.TipoPrato,
+                Ingredientes = info.Ingredientes
+            };
+            return PratoDetail(await _repo.AddPrato(newPrato));
+        }
 
-//             if (Prato == null)
-//             {
-//                 return null;
-//             }
+        private ListarPratoDTO PratoListItem(Prato p) {
+            return new ListarPratoDTO {
+                Nome = p.Nome,
+                IsAtivo = p.IsAtivo,
+                TipoPrato = p.TipoPrato,
+                Ingredientes = p.Ingredientes,
+            };
+        }
 
-//             return DetalharPrato(Prato);
-//         }
-
-//         private ListarPratoDTO PratoListItem(Prato tp) {
-//             return new ListarPratoDTO {
-//                 Id = tp.Id,
-//                 Nome = tp.Nome
-//             };
-//         }
-
-//         private DetalharPratoDTO DetalharPrato (Prato tp) {
-//             return new DetalharPratoDTO {
-//                 Nome = tp.Nome
-//             };
-//         }
-//     }
-// }
+        private ListarPratoDTO PratoDetail(Prato p)
+        {
+            return new ListarPratoDTO
+            {
+                Id = p.Id,
+                Nome = p.Nome,
+                IsAtivo = p.IsAtivo,
+                TipoPrato = p.TipoPrato,
+                Ingredientes = p.Ingredientes
+            };
+        }
+    }
+}
