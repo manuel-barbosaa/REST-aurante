282fdd8e0c7ffb5e8098adb36b790c251c7ee659
Franciscs Soares <1221180@isep.ipp.pt>
2024-10-29 15:12:08 +0000

    id listar ingrediente

--- Cozinha/Model/DTO/ListarIngredienteDTO.cs
+++ Cozinha/Model/DTO/ListarIngredienteDTO.cs
@@ -2,6 +2,7 @@ namespace Cozinha.Model.DTO;
 
 public class ListarIngredienteDTO
 {
+    public int Id { get; set; }
     public required string Nome { get; set; }
     public required string CategoriaAlimenticia { get; set; }
     public bool Ativo { get; set; }
