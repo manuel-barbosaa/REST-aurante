82b310bdf6c4a05883a877137057f5651255bd21
Tatiana Patricia Loureiro Teixeira <1221260@isep.ipp.pt>
2024-10-29 15:53:06 +0000

    feat:  DTO definir tipon de refeição

---
+++ Cozinha/Model/DTO/DefinirTipoRefeicaoDTO.cs
@@ -0,0 +1,7 @@
+namespace Cozinha.Model.DTO;
+
+    public class DefinirTipoRefeicaoDTO{
+         public required string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+    
