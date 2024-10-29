ddf31e253880af3676093028a90faee30735090c
Tatiana Patricia Loureiro Teixeira <1221260@isep.ipp.pt>
2024-10-29 15:51:20 +0000

    feat: tipo de refeição

---
+++ Cozinha/Model/TipoRefeicao.cs
@@ -0,0 +1,11 @@
+namespace Cozinha.Model{
+
+public class TipoRefeicao{
+
+    public long Id { get; set;}
+    public required string Nome { get; set;}
+
+    public string Descricao { get; set; }
+
+}
+}
