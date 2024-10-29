4cdc27c24dbc7880a41fb701cdd7579dc466a05c
Franciscs Soares <1221180@isep.ipp.pt>
2024-10-29 15:06:28 +0000

    alteração variavel Categoria Alimenticia

--- Cozinha/Model/DTO/ListarIngredienteDTO.cs
+++ Cozinha/Model/DTO/ListarIngredienteDTO.cs
@@ -3,6 +3,6 @@ namespace Cozinha.Model.DTO;
 public class ListarIngredienteDTO
 {
     public required string Nome { get; set; }
-    public string CategoriaAlimenticia { get; set; }
+    public required string CategoriaAlimenticia { get; set; }
     public bool Ativo { get; set; }
 }
