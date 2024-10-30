9ab40dad3a9ed5828689c4151e23058218757909
Franciscs Soares <1221180@isep.ipp.pt>
2024-10-30 10:17:55 +0000

    alteração da variavel id ao listar ingrediente dto

--- Cozinha/Model/DTO/ListarIngredienteDTO.cs
+++ Cozinha/Model/DTO/ListarIngredienteDTO.cs
@@ -2,7 +2,7 @@ namespace Cozinha.Model.DTO;
 
 public class ListarIngredienteDTO
 {
-    public required int Id { get; set; }
+    public long Id { get; set; }
     public required string Nome { get; set; }
     public required string CategoriaAlimenticia { get; set; }
     public bool Ativo { get; set; }
