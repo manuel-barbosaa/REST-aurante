7c6d009ad809f26b21032f8fb7456ebb508def02
Franciscs Soares <1221180@isep.ipp.pt>
2024-10-31 11:51:07 +0000

    coerência do repositorio e do service do ingrediente

--- Cozinha/Repositories/IngredienteRepository.cs
+++ Cozinha/Repositories/IngredienteRepository.cs
@@ -1,63 +1,58 @@
 using Cozinha.Model;
 using Microsoft.EntityFrameworkCore;
 
-namespace Cozinha.Repositories
+namespace Cozinha.Repositories;
+
+public class IngredienteRepository
 {
-    public class IngredienteRepository
+    private CozinhaContext _context;
+    
+    public IngredienteRepository(CozinhaContext context)
     {
-        private CozinhaContext _context;
-        public IngredienteRepository(CozinhaContext context)
+        _context = context;
+    }
+
+    public async Task<List<Ingrediente>> GetAllIngredientesFromDataBase()
+    {
+        return await _context.Ingredientes.ToListAsync();
+    }
+
+    public async Task<List<Ingrediente>> GetIngredientesByStateFromDataBase(bool state)
+    {
+        return await _context.Ingredientes.Where(i => i.Ativo == state).ToListAsync();
+    }
+
+    public async Task<Ingrediente?> GetIngredienteById(int id)
+    {
+        return await _context.Ingredientes.FindAsync(id);
+    }
+
+    public async Task<Ingrediente> UpdateIngrediente(Ingrediente ingrediente)
+    {
+        _context.Entry(ingrediente).State = EntityState.Modified;
+
+        try
         {
-            _context = context;
-        }
-
-        public async Task<List<Ingrediente>> GetAllIngredientesFromDataBase()
-        {
-            return await _context.Ingredientes.ToListAsync();
-        }
-
-        public async Task<List<Ingrediente>> GetIngredientesByStateFromDataBase(bool state)
-        {
-            return await _context.Ingredientes.Where(i => i.Ativo == state).ToListAsync();
-        }
-
-
-        public async Task<Ingrediente?> GetIngredienteById(int id)
-        {
-            return await _context.Ingredientes.FindAsync(id);
-        }
-
-
-
-
-        public async Task<Ingrediente> UpdateIngrediente(Ingrediente ingrediente)
-        {
-            _context.Entry(ingrediente).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-                return ingrediente;
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                throw;
-            }
-        }
-
-        public async Task<Ingrediente> AddIngrediente(Ingrediente ingrediente)
-        {
-            var newIngrediente = await _context.Ingredientes.AddAsync(ingrediente);
-
             await _context.SaveChangesAsync();
-            return newIngrediente.Entity;
+            return ingrediente;
         }
-
-        public async Task<bool> Remove(Ingrediente ingrediente)
+        catch (DbUpdateConcurrencyException)
         {
-            _context.Ingredientes.Remove(ingrediente);
-            await _context.SaveChangesAsync();
-            return true;
+            throw;
         }
+    }
+
+    public async Task<Ingrediente> AddIngrediente(Ingrediente ingrediente)
+    {
+        var newIngrediente = await _context.Ingredientes.AddAsync(ingrediente);
+        await _context.SaveChangesAsync();
+        return newIngrediente.Entity;
+    }
+
+    public async Task<bool> Remove(Ingrediente ingrediente)
+    {
+        _context.Ingredientes.Remove(ingrediente);
+        await _context.SaveChangesAsync();
+        return true;
     }
 }
--- Cozinha/Services/Ingrediente/IngredienteService.cs
+++ Cozinha/Services/Ingrediente/IngredienteService.cs
@@ -1,9 +1,9 @@
-using Microsoft.EntityFrameworkCore;
 using Cozinha.Repositories;
 using Cozinha.Model;
 using Cozinha.Model.DTO;
 
 namespace Cozinha.Services;
+
 public class IngredienteService
 {
     private CozinhaContext _context;
@@ -18,41 +18,32 @@ public class IngredienteService
     public async Task<List<ListarIngredienteDTO>> GetIngredientesList()
     {
         List<Ingrediente> allIngredientes = await _repo.GetAllIngredientesFromDataBase();
-
         return allIngredientes.Select(x => IngredienteListItem(x)).ToList();
     }
 
     public async Task<List<ListarIngredienteDTO>> GetIngredientesAtivosList()
     {
         List<Ingrediente> allActiveIngredientes = await _repo.GetIngredientesByStateFromDataBase(true);
-
         return allActiveIngredientes.Select(x => IngredienteListItem(x)).ToList();
     }
 
     public async Task<List<ListarIngredienteDTO>> GetIngredientesInativosList()
     {
         List<Ingrediente> allInactiveIngredientes = await _repo.GetIngredientesByStateFromDataBase(false);
-
         return allInactiveIngredientes.Select(x => IngredienteListItem(x)).ToList();
-
-
     }
 
     public async Task<ListarIngredienteDTO?> GetIngredienteById(int id)
     {
         var ingrediente = await _repo.GetIngredienteById(id);
-
         if (ingrediente == null)
         {
             return null;
         }
-
         return IngredienteDetail(ingrediente);
     }
 
-        }
-
-        public async Task<ListarIngredienteDTO> CreateNewIngrediente(CriarIngredienteDTO info)
+    public async Task<ListarIngredienteDTO> CreateNewIngrediente(CriarIngredienteDTO info)
     {
         Ingrediente newIngrediente = new Ingrediente
         {
@@ -60,8 +51,11 @@ public class IngredienteService
             CategoriaAlimenticia = info.CategoriaAlimenticia,
             Ativo = info.Ativo
         };
-        return IngredienteDetail(await _repo.AddIngrediente(newIngrediente));
+
+        var ingrediente = await _repo.AddIngrediente(newIngrediente);
+        return IngredienteDetail(ingrediente);
     }
+
     private ListarIngredienteDTO IngredienteListItem(Ingrediente i)
     {
         return new ListarIngredienteDTO
