45aa6c377af42bda4917e094d5d559529fbad091
Franciscs Soares <1221180@isep.ipp.pt>
2024-10-31 11:49:59 +0000

    criar e listar ingrediente DTO

--- Cozinha/Model/DTO/CriarIngredienteDTO.cs
+++ Cozinha/Model/DTO/CriarIngredienteDTO.cs
@@ -2,7 +2,7 @@ namespace Cozinha.Model.DTO;
 
 public class CriarIngredienteDTO
 {
-     public long Id { get; set; }
+    public long Id { get; set; } 
     public required string Nome { get; set; }
     public required string CategoriaAlimenticia { get; set; }
     public bool Ativo { get; set; }
