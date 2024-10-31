abda982e8da1cccb45660326823ebe002eaf9d27
manuel barbosa <1220973@isep.ipp.pt>
2024-10-31 12:01:18 +0000

    criação de migrations e implementação da database

---
+++ Cozinha/Migrations/20241031115717_CozinhaCreation.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace REST_aurante.Cozinha.Migrations
+{
+    /// <inheritdoc />
+    public partial class CozinhaCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TipoRefeicao",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoRefeicao", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TiposPrato",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TiposPrato", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pratos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    IsAtivo = table.Column<bool>(type: "INTEGER", nullable: false),
+                    TipoPratoId = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pratos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pratos_TiposPrato_TipoPratoId",
+                        column: x => x.TipoPratoId,
+                        principalTable: "TiposPrato",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredientes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    CategoriaAlimenticia = table.Column<string>(type: "TEXT", nullable: false),
+                    Ativo = table.Column<bool>(type: "INTEGER", nullable: false),
+                    PratoId = table.Column<long>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredientes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ingredientes_Pratos_PratoId",
+                        column: x => x.PratoId,
+                        principalTable: "Pratos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredientes_PratoId",
+                table: "Ingredientes",
+                column: "PratoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pratos_TipoPratoId",
+                table: "Pratos",
+                column: "TipoPratoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ingredientes");
+
+            migrationBuilder.DropTable(
+                name: "TipoRefeicao");
+
+            migrationBuilder.DropTable(
+                name: "Pratos");
+
+            migrationBuilder.DropTable(
+                name: "TiposPrato");
+        }
+    }
+}
