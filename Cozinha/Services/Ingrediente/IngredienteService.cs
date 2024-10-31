05f6747ae5353821a28436919cac4dbe09e0e8eb
Franciscs Soares <1221180@isep.ipp.pt>
2024-10-31 11:24:47 +0000

    get ingrediente by id na classe ingrediente service

--- Cozinha/Services/Ingrediente/IngredienteService.cs
+++ Cozinha/Services/Ingrediente/IngredienteService.cs
@@ -4,67 +4,83 @@ using Cozinha.Model;
 using Cozinha.Model.DTO;
 
 namespace Cozinha.Services;
-    public class IngredienteService
+public class IngredienteService
+{
+    private CozinhaContext _context;
+    private IngredienteRepository _repo;
+
+    public IngredienteService(CozinhaContext context)
     {
-        private CozinhaContext _context;
-        private IngredienteRepository _repo;
+        _context = context;
+        _repo = new IngredienteRepository(_context);
+    }
 
-        public IngredienteService(CozinhaContext context)
+    public async Task<List<ListarIngredienteDTO>> GetIngredientesList()
+    {
+        List<Ingrediente> allIngredientes = await _repo.GetAllIngredientesFromDataBase();
+
+        return allIngredientes.Select(x => IngredienteListItem(x)).ToList();
+    }
+
+    public async Task<List<ListarIngredienteDTO>> GetIngredientesAtivosList()
+    {
+        List<Ingrediente> allActiveIngredientes = await _repo.GetIngredientesByStateFromDataBase(true);
+
+        return allActiveIngredientes.Select(x => IngredienteListItem(x)).ToList();
+    }
+
+    public async Task<List<ListarIngredienteDTO>> GetIngredientesInativosList()
+    {
+        List<Ingrediente> allInactiveIngredientes = await _repo.GetIngredientesByStateFromDataBase(false);
+
+        return allInactiveIngredientes.Select(x => IngredienteListItem(x)).ToList();
+
+
+    }
+
+    public async Task<ListarIngredienteDTO?> GetIngredienteById(int id)
+    {
+        var ingrediente = await _repo.GetIngredienteById(id);
+
+        if (ingrediente == null)
         {
-            _context = context;
-            _repo = new IngredienteRepository(_context);
+            return null;
         }
 
-        public async Task<List<ListarIngredienteDTO>> GetIngredientesList()
-        {
-            List<Ingrediente> allIngredientes = await _repo.GetAllIngredientesFromDataBase();
+        return IngredienteDetail(ingrediente);
+    }
 
-            return allIngredientes.Select(x => IngredienteListItem(x)).ToList();
-        }
-
-        public async Task<List<ListarIngredienteDTO>> GetIngredientesAtivosList()
-        {
-            List<Ingrediente> allActiveIngredientes = await _repo.GetIngredientesByStateFromDataBase(true);
-        
-            return allActiveIngredientes.Select(x => IngredienteListItem(x)).ToList();
-        }
-
-        public async Task<List<ListarIngredienteDTO>> GetIngredientesInativosList()
-        {
-            List<Ingrediente> allInactiveIngredientes = await _repo.GetIngredientesByStateFromDataBase(false);
-        
-            return allInactiveIngredientes.Select(x => IngredienteListItem(x)).ToList();
         }
 
         public async Task<ListarIngredienteDTO> CreateNewIngrediente(CriarIngredienteDTO info)
+    {
+        Ingrediente newIngrediente = new Ingrediente
         {
-            Ingrediente newIngrediente = new Ingrediente
-            {
-                Nome = info.Nome,
-                CategoriaAlimenticia = info.CategoriaAlimenticia,
-                Ativo = info.Ativo
-            };
-            return IngredienteDetail(await _repo.AddIngrediente(newIngrediente));
-        }
-        private ListarIngredienteDTO IngredienteListItem(Ingrediente i)
-        {
-            return new ListarIngredienteDTO
-            {
-                Id = i.Id,
-                Nome = i.Nome,
-                CategoriaAlimenticia = i.CategoriaAlimenticia,
-                Ativo = i.Ativo
-            };
-        }
-
-        private ListarIngredienteDTO IngredienteDetail(Ingrediente i)
-        {
-            return new ListarIngredienteDTO
-            {
-                Id = i.Id,
-                Nome = i.Nome,
-                CategoriaAlimenticia = i.CategoriaAlimenticia,
-                Ativo = i.Ativo
-            };
-        }
+            Nome = info.Nome,
+            CategoriaAlimenticia = info.CategoriaAlimenticia,
+            Ativo = info.Ativo
+        };
+        return IngredienteDetail(await _repo.AddIngrediente(newIngrediente));
     }
+    private ListarIngredienteDTO IngredienteListItem(Ingrediente i)
+    {
+        return new ListarIngredienteDTO
+        {
+            Id = i.Id,
+            Nome = i.Nome,
+            CategoriaAlimenticia = i.CategoriaAlimenticia,
+            Ativo = i.Ativo
+        };
+    }
+
+    private ListarIngredienteDTO IngredienteDetail(Ingrediente i)
+    {
+        return new ListarIngredienteDTO
+        {
+            Id = i.Id,
+            Nome = i.Nome,
+            CategoriaAlimenticia = i.CategoriaAlimenticia,
+            Ativo = i.Ativo
+        };
+    }
+}
