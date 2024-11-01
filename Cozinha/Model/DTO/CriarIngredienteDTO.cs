119d4080b8634bb6af711651795572303a3fb196
Franciscs Soares <1221180@isep.ipp.pt>
2024-11-01 17:59:54 +0000

    variável Ativo required

--- Cozinha/Model/DTO/CriarIngredienteDTO.cs
+++ Cozinha/Model/DTO/CriarIngredienteDTO.cs
@@ -5,5 +5,5 @@ public class CriarIngredienteDTO
     public long Id { get; set; } 
     public required string Nome { get; set; }
     public required string CategoriaAlimenticia { get; set; }
-    public bool Ativo { get; set; }
+    public required bool Ativo { get; set; }
 }
