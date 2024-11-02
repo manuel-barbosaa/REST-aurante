a16e26277441eee0680d2443d7a72a72af125e45
Franciscs Soares <1221180@isep.ipp.pt>
2024-11-02 15:06:49 +0000

    tirar parâmetro id do post

--- Cozinha/Model/DTO/CriarIngredienteDTO.cs
+++ Cozinha/Model/DTO/CriarIngredienteDTO.cs
@@ -2,7 +2,6 @@ namespace Cozinha.Model.DTO;
 
 public class CriarIngredienteDTO
 {
-    public long Id { get; set; } 
     public required string Nome { get; set; }
     public required string CategoriaAlimenticia { get; set; }
     public required bool Ativo { get; set; }
