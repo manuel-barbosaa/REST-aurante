7c7cc2912e2662082c556794771dcfc5c8cdfb2a
Tatiana Patricia Loureiro Teixeira <1221260@isep.ipp.pt>
2024-11-03 14:29:22 +0000

    feat: Controller Ementa

---
+++ Cozinha/Controllers/EmentaController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using Cozinha.Services;
+using Cozinha.Model;
+using Cozinha.Model.DTO;
+using System;
+using System.Threading.Tasks;
+
+namespace Cozinha.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmentaController : ControllerBase
+    {
+        private readonly EmentaService _service;
+
+        public EmentaController(EmentaService service)
+        {
+            _service = service;
+        }
+
+        // GET: api/Ementa/Disponivel?tipo=Diaria&dataInicio=2024-10-30&dataFim=2024-10-30
+        [HttpGet("Disponivel")]
+        public async Task<ActionResult<ListarEmentaDTO>> GetEmentaDisponivel([FromQuery] string tipo, [FromQuery] DateTime dataInicio, [FromQuery] DateTime dataFim)
+        {
+            var ementa = await _service.GetEmentaDisponivel(tipo, dataInicio, dataFim);
+            return ementa == null ? NotFound() : Ok(ementa);
+        }
+
+        // POST: api/Ementa
+        [HttpPost]
+        public async Task<IActionResult> AddEmenta([FromBody] Ementa ementa)
+        {
+            await _service.AddEmenta(ementa);
+            return CreatedAtAction(nameof(GetEmentaDisponivel), new { tipo = ementa.Frequencia, dataInicio = ementa.DataInicio, dataFim = ementa.DataFim }, ementa);
+        }
+        // DELETE: api/Prato/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmenta(long id)
+        {
+            var deleted = await _service.DeleteEmenta(id);
+
+            if (!deleted)
+            {
+                return NotFound(); // Retorna 404 se o prato não for encontrado
+            }
+
+            return NoContent(); // Retorna 204 se o prato for removido com sucesso
+        }
+
+
+
+    }
+}
