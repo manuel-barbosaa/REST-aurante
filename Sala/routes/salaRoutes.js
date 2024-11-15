80b8c0b16684a91791e81c59492c73121321bd30
Tatiana Patricia Loureiro Teixeira <1221260@isep.ipp.pt>
2024-11-15 15:31:31 +0000

    alteração do routes

--- Sala/routes/salaRoutes.js
+++ Sala/routes/salaRoutes.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const clienteController = require('../controllers/clienteController');
 
 router.post('/clientes', clienteController.createCliente);
-router.patch('/clientes/:id/deposit', clienteController.deposit)
+router.patch('/clientes/:nif/deposit', clienteController.deposit)
 router.get('/clientes/', clienteController.getClientes);
 router.get('/:nif', clienteController.getClienteByNIF);
 router.get('/:nif/saldo', clienteController.getClienteSaldo);
