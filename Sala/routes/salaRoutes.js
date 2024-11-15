70a3a6237b8ede3e23de93db1be660ade7b275a5
beamsteixeira <1220824@isep.ipp.pt>
2024-11-15 14:55:06 +0000

    router- getClienteSaldo

--- Sala/routes/salaRoutes.js
+++ Sala/routes/salaRoutes.js
@@ -7,5 +7,6 @@ router.post('/clientes', clienteController.createCliente);
 router.patch('/clientes/:id/deposit', clienteController.deposit)
 router.get('/clientes/', clienteController.getClientes);
 router.get('/:nif', clienteController.getClienteByNIF);
+router.get('/:nif/saldo', clienteController.getClienteSaldo);
 
 module.exports = router;
