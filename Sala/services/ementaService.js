206b75db7577305b2f67bc08c1f350dbb6c12c6c
Tiago Pereira <1221034@isep.ipp.pt>
2024-11-16 12:59:25 +0000

    Atualização do service criar ementa

--- Sala/services/ementaService.js
+++ Sala/services/ementaService.js
@@ -1,11 +1,33 @@
 const webApiClient = require('axios').default;
+const ementaRepository = require("../repositories/ementaRepository")
 
-exports.getEmenta = async function (id) {
+exports.getEmentaCozinha = async function (id) {
     process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;   // Inibe a vericação dos certificados
 
-    const cozinhaWebAPIURL = 'https://localhost:7291/api/Heroes/'
+    const cozinhaWebAPIURL = 'http://localhost:5230/api/Ementa/'
 
     const ementaCozinha = await webApiClient.get(cozinhaWebAPIURL + id);
    
     return ementaCozinha.data;
+}
+
+exports.createEmenta = async function (ementaDTO){
+    const ementaId = ementaDTO.ementaId;
+    const refeicaoId = ementaDTO.refeicaoId;
+    const preco = ementaDTO.preco;
+
+    const ementaCozinha = await exports.getEmentaCozinha(ementaId);
+
+    const refeicao = ementaCozinha.refeicoes.filter(refeicao => refeicao.id === refeicaoId)[0];
+    
+    const prato = refeicao.prato;
+    console.log(prato);
+    console.log(refeicao);
+    console.log(ementaCozinha);
+
+    return await ementaRepository.createEmenta({
+        prato: prato,
+        refeicao: refeicaoId,
+        preco: preco
+    });
 }
