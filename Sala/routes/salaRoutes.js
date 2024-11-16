b99a59d7e7171fe5f7efdba3925536086c3c9382
Tiago Pereira <1221034@isep.ipp.pt>
2024-11-16 12:58:58 +0000

    Update routes sala

--- Sala/routes/salaRoutes.js
+++ Sala/routes/salaRoutes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const clienteController = require('../controllers/clienteController');
+const ementaController = require("../controllers/ementaController");
 
 // Define the routes in the correct order
 router.get('/clientes/:nif', clienteController.getClienteByNIF);  // Specific client by NIF
@@ -9,5 +10,7 @@ router.get('/clientes', clienteController.getClientes);  // All clients or gener
 router.post('/clientes', clienteController.createCliente);
 router.patch('/clientes/:nif/deposit', clienteController.deposit);
 router.get('/clientes/:nif/saldo', clienteController.getClienteSaldo);
+router.post("/ementa", ementaController.createEmenta);
+
 
 module.exports = router;
