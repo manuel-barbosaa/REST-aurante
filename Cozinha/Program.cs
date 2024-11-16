ae33f02307889a6238f8717f75a57a6f97d245e8
manuel barbosa <1220973@isep.ipp.pt>
2024-11-16 14:09:02 +0000

    implementação do Cors na web api de .NET

--- Cozinha/Program.cs
+++ Cozinha/Program.cs
@@ -7,6 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors( options => {
+        options.AddPolicy(name: "MyAllowSpecificOrigins",
+                policy => { policy.WithOrigins("http://localhost:8080");
+                });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
