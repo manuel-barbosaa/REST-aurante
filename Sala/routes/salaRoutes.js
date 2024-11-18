02ea7c08a43fb1b1556c0445ab1f81e21cedb201
Franciscs Soares <1221180@isep.ipp.pt>
2024-11-18 15:13:30 +0000

    definição das rotas do delete cliente by nif e delete all clientes

--- Sala/routes/salaRoutes.js
+++ Sala/routes/salaRoutes.js
@@ -4,12 +4,14 @@ var router = express.Router();
 const clienteController = require('../controllers/clienteController');
 const ementaController = require("../controllers/ementaController");
 
-// Define the routes in the correct order
+
 router.get('/clientes/:nif', clienteController.getClienteByNIF);  // Specific client by NIF
 router.get('/clientes', clienteController.getClientes);  // All clients or general request
 router.post('/clientes', clienteController.createCliente);
 router.patch('/clientes/:nif/deposit', clienteController.deposit);
 router.get('/clientes/:nif/saldo', clienteController.getClienteSaldo);
+router.delete('/clientes/:nif', clienteController.deleteClienteByNIF); 
+router.delete('/clientes', clienteController.deleteAllClientes); 
 router.post("/ementa", ementaController.createEmenta);
 router.get("/ementa/:refeicaoId", ementaController.listarRefeicoesEmenta);
 
