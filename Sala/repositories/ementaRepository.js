ab801213bbe0c0d51553676cc181189a770c9aba
Tatiana Patricia Loureiro Teixeira <1221260@isep.ipp.pt>
2024-11-18 11:45:06 +0000

    feat: Ementa repository-US007 getEmentaByRefeicaoId

--- Sala/repositories/ementaRepository.js
+++ Sala/repositories/ementaRepository.js
@@ -1,3 +1,4 @@
+const ementa = require("../models/ementa");
 const EmentaModel = require("../models/ementa");
 
 
@@ -11,3 +12,24 @@ exports.createEmenta = async function(ementaData){
         throw new Error({message: "repo"});
     }
 }
+
+
+
+exports.getEmentaByRefeicaoId = async function (refeicaoId) {
+    try {
+        // Verifica se refeicaoId é um número válido
+        if (isNaN(Number(refeicaoId))) {
+            throw new Error('O ID da refeição deve ser um número válido.');
+        }
+
+        const ementa = await EmentaModel.find({ refeicaoId: Number(refeicaoId) });
+
+        if (!ementa || ementa.length === 0) {
+            throw new Error('Nenhuma ementa encontrada para o ID da refeição fornecido.');
+        }
+
+        return ementa;
+    } catch (err) {
+        throw new Error(`Erro a procurar ementa atraves do ID: ${err.message}`);
+    }
+};
