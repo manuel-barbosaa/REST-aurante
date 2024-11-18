1e87f370b6650cd6870fa5a02e161af7d61eb12b
Tatiana Patricia Loureiro Teixeira <1221260@isep.ipp.pt>
2024-11-18 11:47:03 +0000

    feat: Ementa controller lisarRefeicoesEmenta

--- Sala/controllers/ementaController.js
+++ Sala/controllers/ementaController.js
@@ -9,3 +9,13 @@ exports.createEmenta= async function (req, res){
         res.status(201).json({message: 'Ementa criada com sucesso'});
     }
 }
+
+exports.listarRefeicoesEmenta= async function (req, res){
+    const result= await EmentaService.listarRefeicoesEmenta(req.params);
+
+    if(!result){
+        res.status(404).send('Erro ao listar ementa');
+    } else{
+        res.status(201).json(result);
+    }
+}
