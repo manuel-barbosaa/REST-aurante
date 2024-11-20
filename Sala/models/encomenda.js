aba34c67dbbb8d4f5d08a1c5dfe91999ee463b7f
Franciscs Soares <1221180@isep.ipp.pt>
2024-11-20 14:36:03 +0000

    novo modelo para representar as encomendas

---
+++ Sala/models/encomenda.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var EncomendaSchema = new Schema({
+    cliente: { type: Schema.Types.ObjectId, ref: 'Cliente', required: true },
+    data: { type: Date, default: Date.now },
+    prato: { type: String, required: true },
+    valor: { type: Number, required: true }
+});
+
+module.exports = mongoose.model('Encomenda', EncomendaSchema);
