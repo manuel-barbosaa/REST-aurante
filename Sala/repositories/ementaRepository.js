8b7c3056f02201ef7e47b636d5b64ebbe7da42d3
Tatiana Patricia Loureiro Teixeira <1221260@isep.ipp.pt>
2024-11-22 15:27:34 +0000

    fix: erros de sintaxe

--- Sala/repositories/ementaRepository.js
+++ Sala/repositories/ementaRepository.js
@@ -37,7 +37,7 @@ exports.getEmentaByRefeicaoId = async function (refeicaoId) {
 
 
 
-exports.deleteClienteById = async function (id) {
+exports.deleteEmentaById = async function (id) {
     try {
         const result = await EmentaModel.deleteOne({ _id: id });
         return result;
