ad6e21c8157e9d51a2acc7c258172386ab67c8ee
Franciscs Soares <1221180@isep.ipp.pt>
2024-11-23 17:25:38 +0000

    listar encomenda cliente controller

--- Sala/controllers/EncomendaController.js
+++ Sala/controllers/EncomendaController.js
@@ -10,3 +10,21 @@ exports.createEncomenda= async function(req, res){
         res.status(201).json({message: 'Encomenda criada com sucesso'});
     }
 }
+
+
+exports.listEncomendasCliente = async function (req, res) {
+    const { nif } = req.params;
+
+    try {
+        const result = await EncomendaService.listEncomendasCliente(nif);
+
+        if (!result || result.length === 0) {
+            return res.status(404).json({ message: "Nenhuma encomenda encontrada para este cliente." });
+        }
+
+        res.status(200).json(result);
+    } catch (error) {
+        console.error("Erro ao listar encomendas do cliente:", error);
+        res.status(500).json({ message: "Erro interno ao listar encomendas." });
+    }
+};
