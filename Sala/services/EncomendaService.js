77567517c5bd54369b804d305b0fb9fe9cc0e746
Tatiana Patricia Loureiro Teixeira <1221260@isep.ipp.pt>
2024-11-23 16:02:07 +0000

    feat: debito do valor depois de cirar encomenda

--- Sala/services/EncomendaService.js
+++ Sala/services/EncomendaService.js
@@ -5,21 +5,27 @@ const clienteRepository= require("../repositories/clienteRepository")
 
 exports.createEncomenda = async function(ementaId, clienteNif){
     
-    const ementa= ementaRepository.getEmentaById(ementaId);
+    const ementa= await ementaRepository.getEmentaById(ementaId);
     const prato= ementa.prato.nome;
     const valor= ementa.preco; 
-    const cliente= clienteRepository.getEmentaByNIF(clienteNif);
+    const cliente= await clienteRepository.getEmentaByNIF(clienteNif);
 
     if(cliente.saldo< valor){
        return false;
     
     }
-    return await ementaRepository.createEncomenda({
+    // Cria a encomenda
+    const novaEncomenda = await ementaRepository.createEncomenda({
         cliente: cliente,
-        data: Date.now,
+        data: Date.now(),
         prato: prato,
         valor: valor
-        
     });
 
-} 
+    const debito = -valor; // O valor é negativo para debitar
+    await clienteRepository.updateSaldo(clienteNif, debito);
+
+
+    return { success: true, encomenda: novaEncomenda };
+};
+
