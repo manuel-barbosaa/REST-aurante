21667be8bc4b120953d9fa14bb32ab7e056014ae
manuel barbosa <1220973@isep.ipp.pt>
2024-11-23 16:55:49 +0000

    implementação do serviço da refeição ao criar encomenda

--- Sala/services/EncomendaService.js
+++ Sala/services/EncomendaService.js
@@ -1,31 +1,58 @@
-
+const webApiClient = require('axios').default;
 const ementaRepository= require("../repositories/ementaRepository")
 const clienteRepository= require("../repositories/clienteRepository")
+const encomendaRepository= require("../repositories/encomendaRepository")
 
+async function servirRefeicaoCozinha(refeicaoId) {
+    process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;   // Inibe a vericação dos certificados
+
+    const cozinhaWebAPIURL = 'http://localhost:5230/api/Refeicao/';
+
+    try {
+
+        const result = await webApiClient.put(`${cozinhaWebAPIURL}${refeicaoId}/servir`);
+
+        if (result.status === 200) {
+            return true;
+        }
+
+        return false;
+    } catch (error) {
+        console.error('Erro ao servir a refeição', error);
+        return false;
+    }
+}
 
 exports.createEncomenda = async function(ementaId, clienteNif){
     
     const ementa= await ementaRepository.getEmentaById(ementaId);
+    console.log(ementa);
     const prato= ementa.prato.nome;
     const valor= ementa.preco; 
-    const cliente= await clienteRepository.getEmentaByNIF(clienteNif);
+    const cliente= await clienteRepository.getClienteByNIF(clienteNif);
 
     if(cliente.saldo< valor){
        return false;
     
     }
-    // Cria a encomenda
-    const novaEncomenda = await ementaRepository.createEncomenda({
-        cliente: cliente,
-        data: Date.now(),
-        prato: prato,
-        valor: valor
-    });
 
-    const debito = -valor; // O valor é negativo para debitar
-    await clienteRepository.updateSaldo(clienteNif, debito);
+    const debito = -valor; 
 
+    const saldoUpdated = await clienteRepository.updateSaldo(clienteNif, debito);
 
-    return { success: true, encomenda: novaEncomenda };
+    if (saldoUpdated) {
+
+        const refeicaoServed = await servirRefeicaoCozinha(ementa.refeicaoId);
+
+        if (refeicaoServed) {
+
+            return await encomendaRepository.createEncomenda({
+                cliente: cliente,
+                data: Date.now(),
+                prato: prato,
+                valor: valor
+            });
+        }
+    }
 };
 
