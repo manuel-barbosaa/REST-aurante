092bbdbb988eff58efe36a3dcb3be6f758f95994
manuel barbosa <1220973@isep.ipp.pt>
2024-11-26 22:21:30 +0000

    correcao de erro no controller da encomenda

--- Sala/controllers/EncomendaController.js
+++ Sala/controllers/EncomendaController.js
@@ -17,7 +17,7 @@ exports.getEncomendas = async function(req, res) {
     if(!result) {
         res.status(404).send('As encomendas não foram encontradas.');
     } else {
-        res.status(200).json(result);
+        res.status(200).json('A encomenda foi criada com sucesso');
     }
 }
 
