1de1ce144deb9c2a3d7e38bb2bd15e82adfefca7
manuel barbosa <1220973@isep.ipp.pt>
2024-11-26 21:38:35 +0000

    Adaptação do repositório das encomendas e modificação de error handling.

--- Sala/repositories/encomendaRepository.js
+++ Sala/repositories/encomendaRepository.js
@@ -4,14 +4,12 @@ const EncomendaModel = require("../models/encomenda");
 
 exports.createEncomenda = async function(encomendaDTO){
     const encomenda  = new EncomendaModel(encomendaDTO);
-    console.log(encomenda);
-    console.log("testN");
     
     try{
         await encomenda.save();
         return true;
     }catch (err){
-        throw new Error({message: "repo"});
+        throw new Error('Não foi possível criar a encomenda. Tente novamente.');
     }
 }
 
@@ -19,32 +17,23 @@ exports.getEncomendas = async function () {
     try {
         return await EncomendaModel.find();
     } catch (error) {
-        console.error("Error fetching encomendas", error);
-        throw new Error("Could not fetch encomendas");
+        throw new Error("Não foi possível procurar as encomendas. Tente novamente.");
     }
 };
 
 exports.getEncomendasByClienteNIF = async function (nif){
     try{
-
         return await EncomendaModel.find({ "cliente.nif": nif}).populate('cliente', 'nif nome').exec();
-
     } catch (err){
-
-        console.error("Erro a ir buscar encomendar por NIF", err);
-
-        return null;
+        throw new Error('Não foi possível procurar a encomenda com o NIF fornecido. Tente novamente.');
     }
    
 };
 
-exports.getClientesByPrato = async function () {
+exports.deleteEncomenda = async function(id) {
     try {
-        const encomendas = await EncomendaModel.find().populate("cliente", "nif nome").exec();
-        return encomendas;
+        return await EncomendaModel.deleteOne({ _id: id});
     } catch (err) {
-        console.error("Não existem encomendas");
-        return null;
+        throw new Error('Não foi possível apagar a encomenda com o ID fornecido. Tente novamente.');
     }
-};
-
+}
