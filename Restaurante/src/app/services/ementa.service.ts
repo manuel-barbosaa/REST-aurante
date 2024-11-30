44220195937b1cff4de96493fbebd0b8fc467d6d
Franciscs Soares <1221180@isep.ipp.pt>
2024-11-30 10:29:48 +0000

    ementa service

--- Restaurante/src/app/services/ementa.service.ts
+++ Restaurante/src/app/services/ementa.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Ementa } from '../Models/ementa';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmentaService {
 
-  constructor() { }
+  private theServerURL = 'http://localhost:8080/api';
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private webApiClient: HttpClient) { }
+
+  // Método para obter a ementa de uma refeição, com base no ID da refeição
+  getEmentaByRefeicaoId(refeicaoId: number): Observable<Ementa> {
+    return this.webApiClient.get<Ementa>(`${this.theServerURL}/ementa/${refeicaoId}`);
+  }
 }
