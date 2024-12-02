316b90d431dd23b8148be2e1b676090e632d8fa6
Tatiana Patricia Loureiro Teixeira <1221260@isep.ipp.pt>
2024-12-02 23:25:37 +0000

    atualização das routes

--- Restaurante/src/app/app.routes.ts
+++ Restaurante/src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
-import { ConsultarEmentasComponent } from './cliente/consultar-ementa/consultar-ementa.component';
-import { EncomendaDetailComponent } from './cliente/encomenda-details/encomenda-detail.component';
-import { ConsultarEncomendasComponent } from './cliente/consultar-encomenda/consultar-encomenda.component';
 import { HomeComponent } from './home/home.component';
+import { ConsultarEmentasComponent } from './cliente/consultar-ementa/consultar-ementa.component';
+import { ConsultarEncomendasComponent } from './cliente/consultar-encomenda/consultar-encomenda.component';
+import { ConsultarClienteComponent } from './cliente/consultar-cliente/consultar-cliente.component';
+import { CarregarContaClienteComponent } from './cliente/carregar-conta-cliente/carregar-conta-cliente.component';
+import { EncomendaDetailComponent } from './cliente/encomenda-details/encomenda-details.component';
 
 export const routes: Routes = [
+ 
   { path: '', component: HomeComponent },  // Rota para a página inicial
   { path: 'ementas', component: ConsultarEmentasComponent },
+  { path: 'consultar-cliente', component: ConsultarClienteComponent },
+  { path: 'carregar-conta/:nif', component: CarregarContaClienteComponent },
+  { path: '', redirectTo: '/consultar-cliente', pathMatch: 'full' },
   { path: 'encomendas', component: ConsultarEncomendasComponent, children: [
       { path: 'encomenda/:id', component: EncomendaDetailComponent },
     ] },
