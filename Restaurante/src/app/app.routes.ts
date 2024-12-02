ce605c96d27097a2dda3fbfe18811a190f33f98b
Franciscs Soares <1221180@isep.ipp.pt>
2024-12-02 16:08:46 +0000

    atualização routes

--- Restaurante/src/app/app.routes.ts
+++ Restaurante/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { ConsultarEmentasComponent } from './cliente/consultar-ementa/consultar-ementa.component';
 import { EncomendaDetailComponent } from './cliente/encomenda-details/encomenda-detail.component';
 import { ConsultarEncomendasComponent } from './cliente/consultar-encomenda/consultar-encomenda.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/ementas', pathMatch: 'full' },
+  { path: '', component: HomeComponent },  // Rota para a página inicial
   { path: 'ementas', component: ConsultarEmentasComponent },
   { path: 'encomendas', component: ConsultarEncomendasComponent, children: [
       { path: 'encomenda/:id', component: EncomendaDetailComponent },
