691aa58ac024c22266ab8898ec90d68c4f098f8b
beamsteixeira <1220824@isep.ipp.pt>
2024-12-03 07:35:21 +0000

    correção de erros no tratamento de exceções

--- Sala/controllers/EncomendaController.js
+++ Sala/controllers/EncomendaController.js
@@ -2,32 +2,47 @@ const EncomendaService = require("../services/EncomendaService");
 
 
 exports.createEncomenda= async function(req, res){
-    const result= await EncomendaService.createEncomenda(req.body.ementaId, req.params.nif);
+    try{
+        const result= await EncomendaService.createEncomenda(req.body.ementaId, req.params.nif);
 
-    if(!result){
-        res.status(404).send('Erro ao criar encomenda');
-    } else{
-        res.status(201).json('Encomenda efetuada com sucesso');
+        if(!result){
+            res.status(404).send('Erro ao criar encomenda');
+        } else{
+            res.status(201).json('Encomenda efetuada com sucesso');
+        }
+    }catch(err){
+        console.error('Error ao criar encomenda:', err.message);
+        res.status(500).json({ error: err.message }); 
     }
 }
 
 exports.getEncomendas = async function(req, res) {
-    const result = await EncomendaService.getEncomendas();
+    try {
+        const result = await EncomendaService.getEncomendas();
 
-    if(!result) {
-        res.status(404).send('As encomendas não foram encontradas.');
-    } else {
-        res.status(200).json(result);
+        if (!result) {
+            res.status(404).send('As encomendas não foram encontradas.');
+        } else {
+            res.status(200).json(result);
+        }
+    }catch(err){
+        console.error('Error ao criar encomenda:', err.message);
+        res.status(500).json({ error: err.message });
     }
 }
 
 exports.getEncomendaById = async function(req, res) {
+    try{
     const result = await EncomendaService.getEncomendaById(req.params.id);
 
-    if(!result) {
+    if (!result) {
         res.status(404).send('A encomenda não foi encontrada.');
     } else {
         res.status(200).json(result);
+    }
+    }catch(err){
+        console.error('Error ao criar encomenda:', err.message);
+        res.status(500).json({ error: err.message });
     }
 }
 
@@ -57,11 +72,16 @@ exports.getPratosComClientes = async function (req, res) {
 };
 
 exports.deleteEncomenda = async function(req, res) {
+    try {
     const result = await EncomendaService.deleteEncomenda(req.params.id);
 
-    if(!result) {
+    if (!result) {
         res.status(400).send('Erro ao apagar encomenda.');
     } else {
         res.status(200).json('Encomenda apagada com sucesso.');
+    }
+    }catch(err){
+        console.error('Error ao criar encomenda:', err.message);
+        res.status(500).json({ error: err.message });
     }
 }
