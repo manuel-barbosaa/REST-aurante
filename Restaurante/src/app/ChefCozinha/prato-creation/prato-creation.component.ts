1233b42c3fdceb537ec6e8b3937413d0377b487d
Tiago Pereira <1221034@isep.ipp.pt>
2024-12-04 15:42:33 +0000

    Criação e desenvolvimento do prato creation

--- Restaurante/src/app/ChefCozinha/prato-creation/prato-creation.component.ts
+++ Restaurante/src/app/ChefCozinha/prato-creation/prato-creation.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PratoService } from '../../services/prato.service';
+import { FormsModule } from '@angular/forms';
+import { NgClass, NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-prato-creation',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgClass, NgIf],
   templateUrl: './prato-creation.component.html',
-  styleUrl: './prato-creation.component.css'
+  styleUrl: './prato-creation.component.css',
 })
-export class PratoCreationComponent {
+export class PratoCreationComponent implements OnInit {
+ 
+    nome: string= '';
+    tipoPrato: string= '';
+    ingredientes: string= '';
+    receita: string= '';
+    isAtivo: string = "";
+  
 
+  alertMessage: string = '';
+  alertClass: string = '';
+
+  constructor(private pratoService: PratoService) {}
+
+  ngOnInit(): void {
+
+  }
+
+  showAlert(message: string, type: 'success' | 'error'): void {
+    this.alertMessage = message;
+    this.alertClass = type;
+    setTimeout(() => {
+      this.alertMessage = '';
+    }, 5000);
+  }
+
+  criarPrato(): void {
+    
+    let newPrato = {
+      nome: this.nome,
+      isAtivo :  this.isAtivo == "true",
+      tipoPrato : parseInt (this.tipoPrato),
+      ingredientes : [parseInt(this.ingredientes)],
+      receita :this.receita
+      
+    }
+    console.log(newPrato);
+    this.pratoService.createPrato(newPrato).subscribe();
+  }
 }
