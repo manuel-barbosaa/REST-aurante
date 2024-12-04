90a3c5ec48f3f71b74cd1e286dcd9071eda1dc87
manuel barbosa <1220973@isep.ipp.pt>
2024-12-04 22:40:50 +0000

    atualização de estrutura e estilos do home component

--- Restaurante/src/app/home/home.component.ts
+++ Restaurante/src/app/home/home.component.ts
@@ -1,43 +1,21 @@
-import { Router } from '@angular/router';
-import {NgFor, NgIf} from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [
-    NgIf
-  ],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgIf],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  mode: string = 'Modo Cliente';  // Inicializa o modo como 'Modo Cliente'
+  mode: string = 'Modo Cliente'; // Default mode
+  isClienteMode: boolean = true; // Default to Cliente mode
 
-  // Funcionalidades do cliente
-  usCliente = [
-    'Carregar a minha conta cliente.',
-    'Consultar a ementa disponível.',
-    'Encomendar um prato existente na ementa.',
-    'Listar todas as encomendas realizadas.',
-  ];
-
-  // Funcionalidades do chef
-  usChef = [
-    'Configurar um prato definindo código único, nome, tipo, ingredientes e receita.',
-    'Ativar/Inativar um prato pelo seu código único.',
-    'Criar uma refeição especificando prato, data, tipo e quantidade.',
-    'Remover uma refeição futura com data posterior à atual.',
-  ];
-
-  // Método para alternar o modo
-  toggleMode(event: any) {
-    this.mode = event.target.checked ? 'Modo Chef' : 'Modo Cliente';
-  }
-
-  // Função para voltar à página inicial
-  goBack() {
-    window.history.back();  // Volta à página anterior
+  toggleMode(event: Event): void {
+    const checkbox = event.target as HTMLInputElement;
+    this.isClienteMode = !checkbox.checked; // If checked, switch to Chef mode
+    this.mode = this.isClienteMode ? 'Modo Cliente' : 'Modo Chef';
   }
 }
