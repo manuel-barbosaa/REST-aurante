227fa27538c6854076c8104c640e559efa4bed10
Tiago Pereira <1221034@isep.ipp.pt>
2024-12-04 17:43:23 +0000

    atualização do model do prato

--- Restaurante/src/app/Models/prato.ts
+++ Restaurante/src/app/Models/prato.ts
@@ -1,4 +1,10 @@
 export interface Prato {
     id: number;
     nome: string;
+    isAtivo: string;
+    tipoPrato: {
+      id: number, nome: string
+    };
+    ingredientes: string;
+    receita: string;
   }
